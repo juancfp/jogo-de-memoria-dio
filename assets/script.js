e9e72a51a6757194ff2231a41507d11f2f73e6ba
Juan Carlos <juancfp.comercial@gmail.com>
2022-06-07 19:19:40 -0300

    Correção de alguns bugs, e botão de reset e shuffle adicionados.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,41 @@
 const cards = document.querySelectorAll('.card');
+const reset = document.getElementById("reset")
+document.getElementById("shuffle").addEventListener("click", shuffle);
+
+
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard=false;
+reset.addEventListener("click", unflipAll);
+reset.setAttribute("disabled","disabled");
 
+// botão de reset
+function unflipAll() {
+    reset.setAttribute("disabled","disabled");
+    cards.forEach((card) => {
+        if(card.classList.contains("flip")) {
+            card.classList.remove("flip");
+            card.addEventListener("click", cardFlip);
+        }
+        
+    });
+}
+
+// botão de shuffle 
+
+function shuffle() { 
+    unflipAll();
+    cards.forEach((card) => { 
+        let ramdom= Math.floor(Math.random() * 12); 
+        card.style.order = ramdom; 
+    });
+}
+// shuffle();
 function cardFlip() {
     if(lockBoard) return;
-    if(this == firstCard) return;
+    if(this == firstCard && hasFlippedCard) {
+        return;
+        }
     this.classList.add('flip');
     if (hasFlippedCard == false){
         hasFlippedCard= true;
@@ -16,7 +46,6 @@ function cardFlip() {
     lockBoard=true;
     checkEqualCards();
     hasFlippedCard = false;
-    lockBoard=false;
 }
 cards.forEach((card) => {
     card.addEventListener('click', cardFlip);
@@ -26,7 +55,7 @@ function checkEqualCards() {
     if(firstCard.dataset.card === secondCard.dataset.card) {
         
         disableCards();
-        
+        reset.removeAttribute("disabled")
         return;
     }
     unflipCards();
@@ -34,13 +63,12 @@ function checkEqualCards() {
 function disableCards() {
     firstCard.removeEventListener('click', cardFlip);
     secondCard.removeEventListener('click', cardFlip);
-    
+    lockBoard=false;
 }
 function unflipCards() {
     setTimeout(() =>{
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
-
         lockBoard=false;
     },1500);
 }
